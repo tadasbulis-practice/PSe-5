107f84defba367a28a468c5ea725975b2b737140
newedgar <edgar.clerc@yahoo.Com>
2026-02-13 10:31:47 +0200

    lab 1 init

---
+++ Lab1/Group.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1;
+
+public class Group
+{
+    public string name { get; set; }
+    private List<StudentProfile> students = new List<StudentProfile>();
+
+    public void addStudent(StudentProfile student)
+    {
+        students.Add(student);
+    }
+
+    public void printAll()
+    {
+        foreach (var student in students)
+        {
+            Console.WriteLine(student.toString());
+        }
+    }
+    
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+StudentProfile student1 = new StudentProfile()
+{
+    FirstName = "edgar",
+    LastName = "clerc",
+    Group = "erasmus",
+    LectureDate = new DateOnly(2026, 02, 12),
+    Grades = [10,12,16,14.5,18.2]
+};
+
+StudentProfile student2 = new StudentProfile()
+{
+    FirstName = "nassim",
+    LastName = "???",
+    Group = "erasmus",
+    LectureDate = new DateOnly(2026, 02, 12),
+    Grades = [2,3,5,2.3]
+};
+
+StudentProfile student3 = new StudentProfile()
+{
+    FirstName = "ibrahim",
+    LastName = "???",
+    Group = "erasmus",
+    LectureDate = new DateOnly(2026, 02, 12),
+    Grades = [18.2,19,17,16.8,18]
+};
+
+Group group = new Group
+{
+    name = "group1"
+};
+
+group.addStudent(student1);
+group.addStudent(student2);
+group.addStudent(student3);
+
+group.printAll();
+
+
+student1.printInfo();
+student2.printInfo();
+student3.printInfo();
