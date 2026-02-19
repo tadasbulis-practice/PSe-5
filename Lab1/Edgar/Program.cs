2f9a38b68840e0eabccca11f79f0aaa359bb83e7
Edgar Clerc <edgar.clerc@yahoo.com>
2026-02-19 15:32:40 +0200

    refacto with contructor

--- Lab1/Edgar/Program.cs
+++ Lab1/Edgar/Program.cs
@@ -1,45 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ConsoleApp1;
+﻿using ConsoleApp1.Edgar.model;
 
-StudentProfile student1 = new StudentProfile()
+namespace ConsoleApp1.Edgar;
+
+internal class Program
 {
-    FirstName = "edgar",
-    LastName = "clerc",
-    Group = "erasmus",
-    LectureDate = new DateOnly(2026, 02, 12),
-    Grades = [10,12,16,14.5,18.2]
-};
+    private static void Main()
+    {
+        var student1 = new StudentProfile(
+            "edgar",
+            "clerc",
+            "erasmus",
+            new DateOnly(2026, 02, 19),
+            [10, 12, 16, 14.5, 18.2]
+        );
 
-StudentProfile student2 = new StudentProfile()
-{
-    FirstName = "nassim",
-    LastName = "???",
-    Group = "erasmus",
-    LectureDate = new DateOnly(2026, 02, 12),
-    Grades = [2,3,5,2.3]
-};
+        var student2 = new StudentProfile(
+            "nassim",
+            "???",
+            "erasmus",
+            new DateOnly(2026, 02, 12),
+            [2, 3, 5, 2.3]
+        );
 
-StudentProfile student3 = new StudentProfile()
-{
-    FirstName = "ibrahim",
-    LastName = "???",
-    Group = "erasmus",
-    LectureDate = new DateOnly(2026, 02, 12),
-    Grades = [18.2,19,17,16.8,18]
-};
+        var student3 = new StudentProfile(
+            "ibrahim",
+            "???",
+            "erasmus",
+            new DateOnly(2026, 02, 12),
+            [18.2, 19, 17, 16.8, 18]
+        );
 
-Group group = new Group
-{
-    name = "group1"
-};
+        var group = new Group("group1");
 
-group.addStudent(student1);
-group.addStudent(student2);
-group.addStudent(student3);
+        group.AddStudent(student1);
+        group.AddStudent(student2);
+        group.AddStudent(student3);
 
-group.printAll();
-
-
-student1.printInfo();
-student2.printInfo();
-student3.printInfo();
+        group.PrintToString();
+    }
+}
