029130a62d6dcd4fdca60d52225a995f2f47ea67
demnatikamaldev <demnatikamaldev@gmail.com>
2026-04-28 05:25:56 +0300

    feature/kamal-Lab6

--- Week-8_Lab-7/Lab7/Legacy/LegacyStudentValidation.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab7.Legacy;
-
-// Pretend this is old code we cannot change
-public class LegacyStudentValidation
-{
-    public bool CheckStudent(string name, string email)
-        => !string.IsNullOrWhiteSpace(name) && email.Contains('@');
-}
--- Week3.LiveCoding.Advanced/Strategies/IGradeStrategy.cs
+++
@@ -1,9 +0,0 @@
-
-using Week3.LiveCoding.Advanced.Models;
-
-namespace Week3.LiveCoding.Advanced.Strategies;
-
-public interface IGradeStrategy
-{
-    double Calculate(Student student);
-}
