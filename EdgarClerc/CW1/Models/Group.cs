954cf83aa16c703f55b4f5c190c5bf4ae3fccc7e
Edgar Clerc <edgar.clerc@yahoo.com>
2026-05-28 12:32:15 +0300

    create model group + student with constructor

---
+++ EdgarClerc/CW1/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿namespace CW1.Models;
+
+public class Group
+{
+    private string Code = "";
+    private string Name = "";
+
+    Group(string code, string name)
+    {
+        Code = code;
+        Name = name;
+    }
+}
---
+++ EdgarClerc/CW1/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace CW1.Models;
+
+public class Student
+{
+    private int _id;
+    private string _name = "";
+    private string _email = "";
+    private string _groupCode = "";
+    private List<int> _grades = new();
+
+    Student(int id, string name, string email, string groupCode, List<int> grades)
+    {
+        _id = id;
+        _name = name;
+        _email = email;
+        _groupCode = groupCode;
+        _grades = grades;
+    }
+
+    public override string ToString()
+    {
+        return $"[{_id}] {_name} ({_groupCode}) email={_email}";
+    }
+}
