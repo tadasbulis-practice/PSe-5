960077c31c1868668eefff64369d7db7d20b2110
Djamel Hammar <109036788+Hammardjamel@users.noreply.github.com>
2026-05-28 11:21:32 +0300

    Remove duplicate root CW1AFTER folder

--- CW1AFTER/Models/Groups.cs
+++
@@ -1,7 +0,0 @@
-// a class reprsent only the data about study group
-namespace CW1After.Models;
-public class Group
-{
-    public string Code { get; set; } = "";
-    public string Name { get; set; } = "";
-}
